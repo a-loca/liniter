895a2938d193271b6951bc5f54dc7f500e899fb5
a-loca <alessandro.locatelli02@gmail.com>
2025-05-13 18:04:18 +0200

    full printing outputs for test.py

--- test.py
+++ test.py
@@ -2,11 +2,19 @@ import liniter.solvers as solvers
 import liniter.utils as utils
 import numpy as np
 import scipy.sparse as sp
+import sys
 
+np.set_printoptions(threshold=sys.maxsize)
 
 A = sp.csr_matrix(np.array([[0, -1, 0], [-1, 3, -1], [0, -1, 4]]))
 # x = np.array([1, 1, 1])
 # b = A.dot(x)
+
+A = utils.load_custom_mtx("test_data/spa1.mtx")
+
+print(A)
+print(A.indices)
+print(A.indptr)
 
 # solver = solvers.GaussSeidelSolver(A, b)
 # sol, _ = solver.solve()
